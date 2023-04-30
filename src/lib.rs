22cbd8a359d910398ec9c66371196c5847a65ef1
Walker Thornley <thornleywalker@gmail.com>
2023-04-30 12:54:43 -0700

    Add typed admin client Actually subscribe to topic in consumer

--- src/lib.rs
+++ src/lib.rs
@@ -3,31 +3,33 @@ pub mod builders;
 use builders::{traits::KafkaConfigBuilder, ConsumerConfigBuilder, ProducerConfigBuilder};
 use futures::{Stream, StreamExt};
 use rdkafka::{
-    consumer::StreamConsumer,
+    admin::{AdminClient, AdminOptions, NewTopic, TopicReplication},
+    client::DefaultClientContext,
+    consumer::{Consumer, StreamConsumer},
     error::KafkaError,
     message::{BorrowedHeaders, BorrowedMessage},
     producer::{FutureProducer, FutureRecord},
     util::Timeout,
     ClientConfig, Message, Timestamp,
 };
-use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use serde::{de::DeserializeOwned, Serialize};
 
 pub trait Topic: Clone {
     type Payload: Serialize + DeserializeOwned;
 
-    fn topic_str(&self) -> String;
+    fn topic_string(&self) -> String;
 }
 
 pub struct TypedMessage<'a, T> {
-    borrowed_message: BorrowedMessage<'a>,
+    message: BorrowedMessage<'a>,
     topic: T,
 }
 impl<'a, T: Topic> TypedMessage<'a, T> {
     pub fn key(&self) -> Option<&[u8]> {
-        self.borrowed_message.key()
+        self.message.key()
     }
     pub fn payload(&self) -> Option<T::Payload> {
-        self.borrowed_message
+        self.message
             .payload()
             .map(|val| serde_json::from_slice(val).unwrap())
     }
@@ -35,19 +37,20 @@ impl<'a, T: Topic> TypedMessage<'a, T> {
         &self.topic
     }
     pub fn partition(&self) -> i32 {
-        self.borrowed_message.partition()
+        self.message.partition()
     }
     pub fn offset(&self) -> i64 {
-        self.borrowed_message.offset()
+        self.message.offset()
     }
     pub fn timestamp(&self) -> Timestamp {
-        self.borrowed_message.timestamp()
+        self.message.timestamp()
     }
     pub fn headers(&self) -> Option<&BorrowedHeaders> {
-        self.borrowed_message.headers()
+        self.message.headers()
     }
 }
 
+#[derive(Clone)]
 pub struct TypedProducer {
     inner: FutureProducer,
 }
@@ -65,7 +68,7 @@ impl TypedProducer {
         timeout: impl Into<Timeout>,
     ) {
         let bytes = serde_json::to_vec(payload).unwrap();
-        let topic_string = topic.topic_str();
+        let topic_string = topic.topic_string();
 
         let record = FutureRecord::to(&topic_string).payload(&bytes);
         let record = if let Some(ref key) = key {
@@ -85,83 +88,120 @@ pub struct TypedConsumer<T> {
 
 impl<T: Topic> TypedConsumer<T> {
     pub fn new(client_config: ClientConfig, topic: T) -> Self {
-        let inner = client_config.create().unwrap();
+        let inner: StreamConsumer = client_config.create().unwrap();
+        inner.subscribe(&[&topic.topic_string()]).unwrap();
 
         Self { inner, topic }
     }
     pub fn topic(&self) -> &T {
         &self.topic
     }
-    pub async fn recv(&self) -> Result<TypedMessage<'_, T>, KafkaError> {
+    pub async fn recv(&self) -> Result<TypedMessage<T>, KafkaError> {
         Ok(TypedMessage {
-            borrowed_message: self.inner.recv().await?,
+            message: self.inner.recv().await?,
             topic: self.topic.clone(),
         })
     }
-    pub async fn stream(&self) -> impl Stream<Item = Result<TypedMessage<'_, T>, KafkaError>> + '_ {
+    pub async fn stream(&self) -> impl Stream<Item = Result<TypedMessage<T>, KafkaError>> + '_ {
         self.inner.stream().map(|val| {
             val.map(|borrowed_message| TypedMessage {
-                borrowed_message,
+                message: borrowed_message,
                 topic: self.topic.clone(),
             })
         })
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-enum Update {
-    Thing1,
-    Thing2,
+pub struct TypedAdmin {
+    inner: AdminClient<DefaultClientContext>,
 }
+impl TypedAdmin {
+    pub fn new(client_config: ClientConfig) -> Self {
+        let inner = client_config.create().unwrap();
 
-#[derive(Clone)]
-struct SessionTopic {
-    id: String,
-}
-impl Topic for SessionTopic {
-    type Payload = Update;
+        Self { inner }
+    }
+    pub async fn create_topic(
+        &self,
+        topic: impl Topic,
+        num_partitions: i32,
+        replication: TopicReplication<'_>,
+    ) {
+        let topic_string = topic.topic_string();
 
-    fn topic_str(&self) -> String {
-        format!("session:{}", self.id)
+        let new_topic = NewTopic::new(&topic_string, num_partitions, replication);
+        let create = self
+            .inner
+            .create_topics(&[new_topic], &AdminOptions::new())
+            .await
+            .unwrap()
+            .first()
+            .unwrap()
+            .as_ref()
+            .unwrap();
     }
 }
 
-async fn _consumer_example() {
-    let config = ConsumerConfigBuilder::new()
-        .bootstrap_servers(&["localhost:9092"])
-        .allow_auto_create_topics(true)
-        .group_id(&format!("user:{}", 123))
-        .build();
+mod example {
+    use serde::{Deserialize, Serialize};
 
-    let consumer = TypedConsumer::new(
-        config,
-        SessionTopic {
-            id: "asdflkj".to_string(),
-        },
-    );
+    use crate::*;
 
-    let mut stream = consumer.stream().await;
-    while let Some(Ok(message)) = stream.next().await {
-        if let Some(payload) = message.payload() {
-            match payload {
-                Update::Thing1 => println!("Do thing 1"),
-                Update::Thing2 => println!("Do thing 2"),
+    #[derive(Debug, Serialize, Deserialize)]
+    enum Update {
+        Thing1,
+        Thing2,
+    }
+
+    #[derive(Clone)]
+    struct SessionTopic {
+        id: String,
+    }
+    impl Topic for SessionTopic {
+        type Payload = Update;
+
+        fn topic_string(&self) -> String {
+            format!("session:{}", self.id)
+        }
+    }
+
+    async fn _consumer_example() {
+        let config = ConsumerConfigBuilder::new()
+            .bootstrap_servers(&["localhost:9092"])
+            .allow_auto_create_topics(true)
+            .group_id(&format!("user:{}", 123))
+            .build();
+
+        let consumer = TypedConsumer::new(
+            config,
+            SessionTopic {
+                id: "asdflkj".to_string(),
+            },
+        );
+
+        let mut stream = consumer.stream().await;
+        while let Some(Ok(message)) = stream.next().await {
+            if let Some(payload) = message.payload() {
+                match payload {
+                    Update::Thing1 => println!("Do thing 1"),
+                    Update::Thing2 => println!("Do thing 2"),
+                }
             }
         }
     }
-}
 
-async fn _producer_example() {
-    let config = ProducerConfigBuilder::new()
-        .bootstrap_servers(&["localhost:9092"])
-        .client_id("client")
-        .build();
+    async fn _producer_example() {
+        let config = ProducerConfigBuilder::new()
+            .bootstrap_servers(&["localhost:9092"])
+            .client_id("client")
+            .build();
 
-    let producer = TypedProducer::new(config);
+        let producer = TypedProducer::new(config);
 
-    let topic = SessionTopic {
-        id: "s2d54f".to_string(),
-    };
+        let topic = SessionTopic {
+            id: "s2d54f".to_string(),
+        };
 
-    producer.send(&topic, &Update::Thing1, None, None).await;
+        producer.send(&topic, &Update::Thing1, None, None).await;
+    }
 }
